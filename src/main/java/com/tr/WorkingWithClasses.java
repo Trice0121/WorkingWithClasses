895910dbed47faf93a4ae708ad2e804a9dd8e9b7
trice012 <trinityrice01@gmail.com>
2023-04-18 15:16:35 -0400

    created a person class

--- src/main/java/com/tr/WorkingWithClasses.java
+++ src/main/java/com/tr/WorkingWithClasses.java
@@ -2,6 +2,40 @@ package com.tr;
 
 public class WorkingWithClasses {
     public static void main(String[] args){
+    Person trinity = new Person();
+    trinity.name = "Trinity";
+    trinity.age = 21;
+    trinity.birthday = "12-19-2001";
+    trinity.hasTattoo = false;
+
+    System.out.println(trinity.name);
+
+
+
+    Person greg = new Person();
 
     }
+}
+
+class Person{
+    // Properties
+    String name;
+    int age;
+    String birthday;
+    boolean hasTattoo;
+
+
+
+
+    // Constructor
+
+
+    // Methods
+
+    // Fruits
+
+
+
+
+
 }
